05139c06b751292472122eaa394a26bbbf03dc54
Sam <91444419+samanthapaigee@users.noreply.github.com>
2022-11-12 16:18:51 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,14 @@
 # bot.py
 
 import discord
+from discord.ext import commands
 import json
 import requests
 import random
 import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
-DISCORD_TOKEN = '[Bot Token]'
-DISCORD_GUILD = '[Server Name]'
 
 client = discord.Client(intents=discord.Intents.all())
 PREFIX: str = '$'
@@ -18,7 +16,7 @@ PREFIX: str = '$'
 
 @client.event
 async def on_ready():
-    guild = discord.utils.get(client.guilds, name=DISCORD_GUILD)
+    guild = discord.utils.get(client.guilds, name=os.getenv('DISCORD_GUILD'))
     print (
         f'{client.user} is connected to the following server:\n'
         f'{guild.name}'
@@ -30,7 +28,7 @@ async def on_message(message):
         return
 
     if message.content.startswith('$pet'):
-        await message.channel.send('bro I am literally a cat, leave me alone')
+        await message.channel.send(os.getenv('PET_PHRASES').split('/')[random.randint(0,2)])
 
 
-client.run(DISCORD_TOKEN)
+client.run(os.getenv('DISCORD_TOKEN'))
