87d1bc27f784d209de35beab74fc02875050366d
Sam <sam.law9973@gmail.com>
2025-05-12 15:24:32 -0400

    Update bot.py
    modernizin' and cleanin'

--- bot.py
+++ bot.py
@@ -1,34 +1,28 @@
 # bot.py
 
-import discord
-from discord.ext import commands
 import json
-import requests
 import random
 import os
 from dotenv import load_dotenv
 
+import discord
+from discord.ext import commands
+
 load_dotenv()
-
-client = discord.Client(intents=discord.Intents.all())
-PREFIX: str = '$'
-
+TOKEN = os.getenv('DISCORD_TOKEN')
+intents = discord.Intents.default()
+intents.message_content = True
+client = commands.Bot(command_prefix='$', intents=intents)
 
 @client.event
 async def on_ready():
     guild = discord.utils.get(client.guilds, name=os.getenv('DISCORD_GUILD'))
     print (
-        f'{client.user} is connected to the following server:\n'
-        f'{guild.name}'
+        f'{client.user} is connected!'
     )
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$pet'):
-        await message.channel.send(os.getenv('PET_PHRASES').split('/')[random.randint(0,2)])
-
+@client.command(name='pet')
+async def pet(ctx):
+    await ctx.send(os.getenv('PET_PHRASES').split(',')[random.randint(0,3)])
 
 client.run(os.getenv('DISCORD_TOKEN'))
